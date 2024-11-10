a4ed121ae23f5c2825562bd64a3d121fa835e0da
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-11-11 00:51:13 +0500

    learning

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-function abcd() {
+// function abcd() {
+//     return 12;
+// }
+// var ans = abcd();
+// var blob = await fetch(`https://randomuser.me/api/`);
+// var res = await blob.json();
+// console.log(res);
+// if the code runs line by line then it is synchronous
+// 
 
-    return 12;
+async function myFunc() {
+    var blob = await fetch(`https://randomuser.me/api/`);
+    var ans = await blob.json();
+
+    console.log(ans.results[0].name);
 }
 
-
-
-var ans = abcd();
+myFunc();
